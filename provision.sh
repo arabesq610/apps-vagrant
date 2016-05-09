bf45abfd9a070238ec812bd6e29550f9164af23e
Elise Linn <elise@gofastpay.com>
2016-05-08 22:00:09 -0700

    Update provision script to remove default nginx web root, and create a symlink to the src folder for now

--- provision.sh
+++ provision.sh
@@ -1,4 +1,14 @@
+# Update packages
 apt-get -y update
-apt-get -y install nginx
-service nginx start
 
+# Install nginx
+apt-get -y install nginx
+
+# Remove default nginx web root
+rm -rf /usr/share/nginx/www
+
+# Create symlink to shared loc
+ln -s /vagrant/src /usr/share/nginx/www
+
+# Start nginx
+service nginx start
